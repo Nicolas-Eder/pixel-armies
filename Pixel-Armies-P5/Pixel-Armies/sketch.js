1bf08e99de9de3790b1f0f646b85deabb8d9c109
Nicolas-Eder <nicolas.eder.1998@gmail.com>
2020-04-22 21:41:29 +0200

    Removed Army class; All Soldiers are now in one array; Soldiers move in fixed direction set at spawn; Soldiers are spawned by clicking.

--- Pixel-Armies-P5/Pixel-Armies/sketch.js
+++ Pixel-Armies-P5/Pixel-Armies/sketch.js
@@ -1,8 +1,7 @@
 'use strict';
 var bgcolor;
 var soldiersize;
-let red_army;
-let blue_army;
+var soldiers = [];
 
 function setup() {
   frameRate(2);
@@ -11,56 +10,44 @@ function setup() {
   
   bgcolor = color(100);
   soldiersize = 10;
-  red_army = new Army(color(255,0,0));
-  blue_army = new Army(color(0,0,255));
 }
 
 function draw() {
-  print(red_army.soldiers.length);
-  for(let i = 0; i < red_army.soldiers.length; i++){
-    red_army.soldiers[i].Hide();
-    red_army.soldiers[i].Move(soldiersize, soldiersize);
-    red_army.soldiers[i].Show();
+  print(soldiers.length);
+  for(let i = 0; i < soldiers.length; i++){
+    soldiers[i].Hide();
+    soldiers[i].Move();
+    soldiers[i].Show();
   }
 }
 
 function mouseClicked(){
-    red_army.SpawnSoldier(mouseX-(soldiersize*1.5), mouseY-soldiersize*1.5);
- // else
-   // blue_army.SpawnSoldier(mouseX-(soldiersize*1.5), mouseY-soldiersize*1.5);
-}
-
-class Army{
-  constructor(color){
-    this.color = color;
-    this.soldiers = [];
-  }
-  SpawnSoldier(x,y){
-    this.soldiers.push(new Soldier(x,y,this.color));
-  }
+  var spawnpos = createVector(mouseX-(soldiersize*1.5), mouseY-soldiersize*1.5);
+  var movedir = createVector(soldiersize,0);
+  var newSoldier = new Soldier(spawnpos, movedir, color(255,0,0));
+  soldiers.push(newSoldier);
 }
 
 class Soldier{
-    constructor(x,y,color){
-      this.x = x;
-      this.y = y;
+    constructor(coords, movedir, color){
+      this.coords = coords;
+      this.movedir = movedir;
       this.color = color;
     }
-    
+
     FightOrMove(x,y){
       
     }
-    Move(x,y){
-      this.x+=x;
-      this.y+=y;
+    Move(){
+      this.coords.add(this.movedir);
     }
     Show(){
       fill(this.color);
-      rect(this.x, this.y, soldiersize, soldiersize);
+      rect(this.coords.x, this.coords.y, soldiersize, soldiersize);
     }
     Hide(){
      fill(bgcolor);
      stroke(bgcolor);
-     rect(this.x, this.y, soldiersize, soldiersize);
+     rect(this.coords.x, this.coords.y, soldiersize, soldiersize);
     }
-  }
+}
