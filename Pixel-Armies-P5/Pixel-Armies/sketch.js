3ab482ef0e522f76d9bb977efccdb080b4ffa941
Nicolas-Eder <nicolas.eder.1998@gmail.com>
2020-04-24 19:30:31 +0200

    Upped framerate and adjusted soldier movement per frame accordingly. Added title to .html. Added .css file.

--- Pixel-Armies-P5/Pixel-Armies/sketch.js
+++ Pixel-Armies-P5/Pixel-Armies/sketch.js
@@ -1,31 +1,49 @@
 'use strict';
 var bgcolor;
-var soldiersize;
+var soldiersize = 10;
 var soldiers = [];
 
 function setup() {
-  frameRate(2);
+  //frameRate(2);
   createCanvas(1000,500);
-  background(100);
-  
   bgcolor = color(100);
-  soldiersize = 10;
 }
 
 function draw() {
-  print(soldiers.length);
+  //Background image
+  background(100);
+  stroke('blue');
+  line(499,0,499,500);
+  stroke('red');
+  line(502,0,502,500);
+  
+  //Soldiers
   for(let i = 0; i < soldiers.length; i++){
-    soldiers[i].Hide();
+    //soldiers[i].Hide();
     soldiers[i].Move();
     soldiers[i].Show();
   }
+  
+  //mouse indicator
+  stroke('black');
+  noFill();
+  var rastercoords = rasterPositionFromMouse();
+  rect(rastercoords.x, rastercoords.y, soldiersize, soldiersize);
 }
 
 function mouseClicked(){
-  var spawnpos = createVector(mouseX-(soldiersize*1.5), mouseY-soldiersize*1.5);
-  var movedir = createVector(soldiersize,0);
+  var spawnpos = rasterPositionFromMouse();
+  
+  //Decide side, default red
+  var movedir = createVector(-1,0);
   var newSoldier = new Soldier(spawnpos, movedir, color(255,0,0));
+  if(spawnpos.x <= 500){//blue
+    newSoldier.movedir.x = newSoldier.movedir.x*-1;
+    newSoldier.color = color(0,0,255);
+  }
+  
   soldiers.push(newSoldier);
+  newSoldier.Show();
 }
 
 class Soldier{
@@ -33,21 +51,40 @@ class Soldier{
       this.coords = coords;
       this.movedir = movedir;
       this.color = color;
+      this.speed = .01;
     }
-
     FightOrMove(x,y){
       
     }
     Move(){
-      this.coords.add(this.movedir);
+      this.coords.x += this.movedir.x*this.speed*deltaTime;
+      this.coords.y += this.movedir.y*this.speed*deltaTime;
     }
     Show(){
       fill(this.color);
-      rect(this.coords.x, this.coords.y, soldiersize, soldiersize);
+      stroke(bgcolor);
+      var pos = this.RoundedPos(this.coords.x, this.coords.y);
+      rect(pos.x, pos.y, soldiersize, soldiersize);
     }
     Hide(){
-     fill(bgcolor);
-     stroke(bgcolor);
-     rect(this.coords.x, this.coords.y, soldiersize, soldiersize);
+      fill(bgcolor);
+      stroke(bgcolor);
+      var pos = this.RoundedPos(this.coords.x, this.coords.y);
+      rect(pos.x, pox.y, soldiersize, soldiersize);
     }
+    RoundedPos(){
+      return createVector(roundpixel(this.coords.x), roundpixel(this.coords.y));
+    }
+}
+
+//Returns the coordinates of a field on the raster. Dependent on mouse position.
+function rasterPositionFromMouse(){
+  return createVector(roundpixel(mouseX-soldiersize*1.5), roundpixel(mouseY-soldiersize*1.5));
+}
+
+//Rundet auf die nächste 10. Also von 32 auf 30 z.B.
+function roundpixel(pixel){
+  var decimal = pixel/10;
+  var rounded = round(decimal);
+  return rounded*10;
 }
